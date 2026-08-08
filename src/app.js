ae45bd0a21c2602aa6367ce8e366fdaac5650f4f
Prashant <pk.ashu98@gmail.com>
2026-08-08 17:15:23 +0530

    exploring validator library

--- src/app.js
+++ src/app.js
@@ -7,10 +7,24 @@ const port = 3000;
 
 app.use(express.json());
 
-app.patch('/user', async (req, res) => {
-    try {
+// Update data of the user
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params?.userId;
+    const data = req.body;
 
-        const userPatch = await User.findByIdAndUpdate({ _id: req.body.userId }, { $set: req.body }, {
+    try {
+        const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+        const isUpdateAllowed = Object.keys(data).every((k) =>
+            ALLOWED_UPDATES.includes(k)
+        );
+        if (!isUpdateAllowed) {
+            throw new Error("Update not allowed");
+        }
+        if (data?.skills.length > 10) {
+            throw new Error("Skills cannot be more than 10");
+        }
+
+        const userPatch = await User.findByIdAndUpdate({ _id: userId }, { $set: data }, {
             returnDocument: "after",
             runValidators: true,
         });
--- src/models/user.js
+++ src/models/user.js
@@ -1,54 +1,70 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      required: true,
-      minLength: 4,
-      maxLength: 50,
+    {
+        firstName: {
+            type: String,
+            required: true,
+            minLength: 4,
+            maxLength: 50,
+        },
+        lastName: {
+            type: String,
+        },
+        emailId: {
+            type: String,
+            lowercase: true,
+            required: true,
+            unique: true,
+            trim: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error("Enter valid email id")
+                }
+            }
+        },
+        password: {
+            type: String,
+            required: true,            
+            validate(value) {
+                if (!validator.isStrongPassword(value)) {
+                    throw new Error("Enter strong password")
+                }
+            }
+        },
+        age: {
+            type: Number,
+            min: 18,
+        },
+        gender: {
+            type: String,
+            validate(value) {
+                if (!["male", "female", "others"].includes(value)) {
+                    throw new Error("Gender data is not valid");
+                }
+            },
+        },
+        photoUrl: {
+            type: String,
+            default: "https://geographyandyou.com/images/user-profile.png",            
+            validate(value) {
+                if (!validator.isURL(value)) {
+                    throw new Error("Enter valid photo url id")
+                }
+            }
+        },
+        about: {
+            type: String,
+            default: "This is a default about of the user!",
+        },
+        skills: {
+            type: [String],
+        },
     },
-    lastName: {
-      type: String,
-    },
-    emailId: {
-      type: String,
-      lowercase: true,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    age: {
-      type: Number,
-      min: 18,
-    },
-    gender: {
-      type: String,
-      validate(value) {
-        if (!["male", "female", "others"].includes(value)) {
-          throw new Error("Gender data is not valid");
-        }
-      },
-    },
-    photoUrl: {
-      type: String,
-      default: "https://geographyandyou.com/images/user-profile.png",
-    },
-    about: {
-      type: String,
-      default: "This is a default about of the user!",
-    },
-    skills: {
-      type: [String],
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 const User = mongoose.model('User', userSchema);
